e1c7748c2a266b4bed7386cba63d8e6bc2ed3034
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 20:08:29 +0530

    commit

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ app.use(express.json());
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'https://bajaj-frontend-smoky-zeta.vercel.app/',
-    methods: 'GET, POST',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: 'https://bajaj-frontend-smoky-zeta.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
 }));
 
 
