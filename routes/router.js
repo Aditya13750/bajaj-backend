4a387154cc5ce2123892652e73042678850253ae
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 18:37:54 +0530

    commit

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require("cors"); // Import cors package
 const router = express.Router();
 const { processData, getOperationCode } = require("../controllers/bfhlController");
 
+const app = express();
+app.use(cors());
+
 router.get('/', (req, res) => {
     res.send("the server is working fine.");
-})
+});
 router.post("/bfhl", processData);
 router.get("/bfhl", getOperationCode);
 
 module.exports = router;
-
