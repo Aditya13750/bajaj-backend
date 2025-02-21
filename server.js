2fd6510cb665b1b6f394b82ecb80fd4a15dcbe62
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 18:40:55 +0530

    server

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const connectWithDB = require('./config/db');
-const app = express();
 const cors = require('cors');
 const router = require('./routes/router');
 require("dotenv").config();
 
+const app = express();
+
+
 connectWithDB();
 
-app.use(cors());
+
+const corsOptions = {
+    origin: process.env.frontendURL, // Allow frontend domain
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+};
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 
+app.options("*", cors(corsOptions));
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
+
 app.use("/", router);
 
-
-app.listen(process.env.port, () => {
-    console.log(`the app is running on ${process.env.port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`The app is running on port ${PORT}`);
 });
