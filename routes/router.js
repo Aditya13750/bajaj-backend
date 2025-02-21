9002368842ce9162a881071085a1d00821d8bf6d
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 18:24:14 +0530

    commit

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { processData, getOperationCode } = require("../controllers/bfhlController");
 
+router.post('/', (req, res) => {
+    res.send("the server is working fine.");
+})
 router.post("/bfhl", processData);
 router.get("/bfhl", getOperationCode);
 
