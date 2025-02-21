d2717d46e1e1485c08249dd876481feb47732ee8
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 19:05:34 +0530

    c

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const cors = require("cors"); // Import cors package
 const router = express.Router();
 const { processData, getOperationCode } = require("../controllers/bfhlController");
-
-const app = express();
-app.use(cors());
 
 router.get('/', (req, res) => {
     res.send("the server is working fine.");
