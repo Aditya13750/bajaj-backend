ca8e37cd4a139a52f3cb0b6c255f770a6a2d6b10
Aditya Giri <adityagiri83853@gmail.com>
2025-02-21 18:25:33 +0530

    .

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { processData, getOperationCode } = require("../controllers/bfhlController");
 
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send("the server is working fine.");
 })
 router.post("/bfhl", processData);
